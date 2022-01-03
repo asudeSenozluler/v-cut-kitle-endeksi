a43cf8b1a6c7dea20d6ec8af59ed5dd40be65d88
Asude Şenözlüler <senozlulerasude@gmail.com>
2022-01-03 14:40:31 +0300

    first commit

---
+++ vücut kitle endeksi/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace vücut_kitle_endeksi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtboy = new System.Windows.Forms.TextBox();
+            this.txtkilo = new System.Windows.Forms.TextBox();
+            this.rbkadın = new System.Windows.Forms.RadioButton();
+            this.rberkek = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtboy
+            // 
+            this.txtboy.Location = new System.Drawing.Point(229, 26);
+            this.txtboy.Name = "txtboy";
+            this.txtboy.Size = new System.Drawing.Size(209, 30);
+            this.txtboy.TabIndex = 0;
+            // 
+            // txtkilo
+            // 
+            this.txtkilo.Location = new System.Drawing.Point(229, 97);
+            this.txtkilo.Name = "txtkilo";
+            this.txtkilo.Size = new System.Drawing.Size(209, 30);
+            this.txtkilo.TabIndex = 1;
+            // 
+            // rbkadın
+            // 
+            this.rbkadın.AutoSize = true;
+            this.rbkadın.Location = new System.Drawing.Point(229, 133);
+            this.rbkadın.Name = "rbkadın";
+            this.rbkadın.Size = new System.Drawing.Size(80, 27);
+            this.rbkadın.TabIndex = 2;
+            this.rbkadın.TabStop = true;
+            this.rbkadın.Text = "Kadın";
+            this.rbkadın.UseVisualStyleBackColor = true;
+            // 
+            // rberkek
+            // 
+            this.rberkek.AutoSize = true;
+            this.rberkek.Location = new System.Drawing.Point(354, 133);
+            this.rberkek.Name = "rberkek";
+            this.rberkek.Size = new System.Drawing.Size(84, 27);
+            this.rberkek.TabIndex = 3;
+            this.rberkek.TabStop = true;
+            this.rberkek.Text = "Erkek";
+            this.rberkek.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(170, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Boy:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Kilo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Cinsiyet:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(145, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 40);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(105, 98);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(211, 23);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Vücut Kitle Endeksiniz:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(229, 224);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 30);
+            this.textBox1.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(300, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 40);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(456, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 23);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Örnek:1,75";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(170, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(283, 23);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "*Lütfen boyunuzu virgüllü girin!";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(393, 224);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(185, 153);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(590, 389);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rberkek);
+            this.Controls.Add(this.rbkadın);
+            this.Controls.Add(this.txtkilo);
+            this.Controls.Add(this.txtboy);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtboy;
+        private System.Windows.Forms.TextBox txtkilo;
+        private System.Windows.Forms.RadioButton rbkadın;
+        private System.Windows.Forms.RadioButton rberkek;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ vücut kitle endeksi/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vücut_kitle_endeksi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double boy, kilo,boykare;
+            double sonuç;
+            boy = Convert.ToDouble(txtboy.Text);
+            kilo = Convert.ToDouble(txtkilo.Text);
+            boykare = Math.Pow(boy, 2);
+            sonuç = kilo / boykare;
+            textBox1.Text = sonuç.ToString();
+
+            if (sonuç > 0 && sonuç < 18)
+            {
+                richTextBox1.Text = "";
+                richTextBox1.Text = "Vücut kitle indeksiniz normalin altında doktor kontrolünde ideal kilonuza ulaşabilirsiniz!";
+            }
+            else if (sonuç > 18 && sonuç < 25)
+            {
+                richTextBox1.Text = "";
+                richTextBox1.Text = "Vücut kitle indeksiniz normal günlük aktivitelerle ideal kilonuzu koruyabilirsiniz!";
+            }
+            else if (sonuç > 25 && sonuç < 30)
+            {
+                richTextBox1.Text = "";
+                richTextBox1.Text = "Vücut kitle indeksiniz normalin üstünde doktor kontrolünde ideal kilonuza ulaşabilirsiniz!";
+            }
+            else if (sonuç > 30 && sonuç < 40)
+            {
+                richTextBox1.Text = "";
+                richTextBox1.Text = "Vücut kitle indeksiniz normalin üstünde en kısa sürede doktorunuzla görüşüp ideal kilonuza ulaşmalısınız!";
+            }
+            else if (sonuç > 40)
+            {
+                richTextBox1.Text = "";
+                richTextBox1.Text = "Vücut kitle indeksiniz normalin çok üstünde en kısa zamanda  doktor kontrolünde ideal kilonuza ulaşamalısınız!";
+            }
+            
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtkilo.Text = "";
+            txtboy.Text = "";
+            richTextBox1.Text = "";
+            textBox1.Text = "";
+        }
+
+       
+    }
+}
